3e5e0e2ec8437ffbcff1dc173ad1341078e95410
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-02 13:08:39 -0400

    Fixed class names

--- FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 import java.util.Scanner;
 
-public class Main {
+public class FullNameApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
