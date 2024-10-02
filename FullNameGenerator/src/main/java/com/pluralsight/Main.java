3309298c77dd2fb40d61ac250f3f8dba44c73dac
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-02 11:56:54 -0400

    Compleated the FullNameGenerator

--- FullNameGenerator/src/main/java/com/pluralsight/Main.java
+++ FullNameGenerator/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,36 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        // First name here
+        System.out.print("Please enter your name\n" +
+                "First name: ");
+        String firstName = scanner.nextLine();
+
+        // Middle name
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine();
+
+        // Last name
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine();
+
+        // Suffix
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine();
+
+
+        // trim and output full name
+        firstName.trim(); middleName.trim(); lastName.trim(); suffix.trim();
+
+        System.out.println("Full name: " +
+                firstName + ' ' +
+                (middleName.isBlank() ? "" : middleName + ' ') +
+                lastName +
+                (suffix.isBlank() ? "" : ", " + suffix));
+
     }
 }
