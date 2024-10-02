bcc9164afb4508c8b2d0fb55b471d595301a5723
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-02 15:51:19 -0400

    HighScoreWins Handles Ties

--- HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
@@ -6,9 +6,12 @@ public class HighScoreWins {
     public static void main(String[] args) {
         String[] gameScore = askForString("Please enter a game score: ").split("[:|]");
 
-        System.out.println("Winner: " +
-                (Integer.valueOf(gameScore[2]) > Integer.valueOf(gameScore[3]) ?
-                        gameScore[0] : gameScore[1]));
+        // Handles a tie by saying the winner is Tie!
+        String winner = (Integer.valueOf(gameScore[2]) == Integer.valueOf(gameScore[3]) ?
+                "Tie!" : (Integer.valueOf(gameScore[2]) > Integer.valueOf(gameScore[3]) ?
+                gameScore[0] : gameScore[1]));
+
+        System.out.println("Winner: " + winner);
     }
     static String askForString(String question) {
         System.out.print(question);
---
+++ TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class TheaterReservations {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
