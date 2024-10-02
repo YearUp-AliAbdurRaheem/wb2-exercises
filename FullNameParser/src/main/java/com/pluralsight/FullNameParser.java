8ff1ee4952b93d7095403dc65c2d62b1aecc0154
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-02 13:27:03 -0400

    Completed FullNameParser

--- FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -2,9 +2,23 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class FullNameParser {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String[] name = askForString("Please enter your name: ").split(" ");
 
+        System.out.printf("""
+                First name:\s%s
+                Middle name:\s%s
+                Last name:\s%s""",
+                name[0],
+                (name.length > 2) ? name[1] : "(none)",
+                (name.length > 2) ? name[2] : name[1]);
 
+    }
+
+    static String askForString(String question) {
+        System.out.print(question);
+        return scanner.nextLine().trim();
     }
 }
