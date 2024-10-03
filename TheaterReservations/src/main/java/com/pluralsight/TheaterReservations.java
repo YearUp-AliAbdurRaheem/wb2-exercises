2140402ca098d5f1c7daa00389ed3f3c5befce3b
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-03 08:53:06 -0400

    Formating

--- TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
+++ TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
@@ -10,9 +10,11 @@ public class TheaterReservations {
         String[] name = askForString("Please enter your name: ").split("\\s+");
         String date = askForString("What date will you be coming (MM/dd/yyyy): ");
         String ticketCount = askForString("How many tickets would you like? ");
+
         // Parsing Date
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         LocalDate reservation = LocalDate.parse(date, formatter);
+
         // Outputting reservation receipt
         System.out.println(ticketCount + " ticket" + ((Integer.valueOf(ticketCount) > 1) ? 's' : "") +
                 " reserved for " + reservation +
