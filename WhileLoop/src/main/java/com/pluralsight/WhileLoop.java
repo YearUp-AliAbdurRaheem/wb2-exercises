a555c4320811a5081a294ee6f0058ddda7ebdab0
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-04 12:01:44 -0400

    Completed Loops

---
+++ WhileLoop/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i > 0) {
+            System.out.println("I love Java");
+            System.out.println("I hate Java"); i--;
+        }
+        System.out.println("I love Coffee");
+    }
+}
