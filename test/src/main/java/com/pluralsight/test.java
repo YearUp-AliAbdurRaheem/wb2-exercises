4040641b4e3462fbda9171d296fd7a9aa08f170f
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-08 18:22:30 -0400

    things occurred

--- test/src/main/java/com/pluralsight/test.java
+++ test/src/main/java/com/pluralsight/test.java
@@ -3,6 +3,9 @@ package com.pluralsight;
 public class test {
 
     public static void main(String[] args){
-        System.out.println("HI");
+        //System.out.println("HI");
+        int x = 5;
+        int y = ++x;
+        System.out.println(y);
     }
 }
