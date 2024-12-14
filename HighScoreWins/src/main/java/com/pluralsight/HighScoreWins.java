8fc1964a9d4d5a1acbb2d1ba7de43fa5b1ca3703
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-12-13 21:20:07 -0500

    explain format

--- HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class HighScoreWins {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        String[] gameScore = askForString("Please enter a game score: ").split("[:|]");
+        String[] gameScore = askForString("Please enter a game score: ").split("[:|]"); // The format is Team1:Team2|Team1Score:Team2Score for example Tigers:Sharks|10:8
 
         // Handles a tie by saying Tie!
         String winner = (Integer.valueOf(gameScore[2]) == Integer.valueOf(gameScore[3]) ?
